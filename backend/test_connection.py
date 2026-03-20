f8539fd75c1d4a67f2f2698d0f2560f62db62700
Pedro Moraes <pedro693.ph@gmail.com>
2026-03-20 16:53:10 -0300

    feat: criação inicial da estrutura de conexão com banco de dados do supabase

---
+++ backend/test_connection.py
@@ -0,0 +1,8 @@
+from database import engine
+
+try:
+    connection = engine.connect()
+    print('conectado ao banco')
+    connection.close()
+except Exception as e:
+    print("erro ao conectar: ", e)
